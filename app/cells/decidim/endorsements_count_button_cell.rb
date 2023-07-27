ab5e09838ae0425a2ce92b4b8d628f35c5934903
Alexander Rusa <alex@rusa.at>
2023-07-27 20:28:32 +0000

    Make endorsements button dynamic with js

---
+++ app/cells/decidim/endorsements_count_button_cell.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Decidim
+  # This cell renders the endrosement button and the endorsements count.
+  # It only supports one row of buttons per page due to current tag ids used by javascript.
+  class EndorsementsCountButtonCell < Decidim::ViewModel
+    include LayoutHelper
+    include CellsHelper
+    include EndorsableHelper
+    include ResourceHelper
+    include TooltipHelper
+    include Decidim::SanitizeHelper
+
+    delegate :current_user, to: :controller, prefix: false
+    delegate :current_settings, to: :controller, prefix: false
+    delegate :current_component, to: :controller, prefix: false
+    delegate :allowed_to?, to: :controller, prefix: false
+
+    def show
+      render
+    end
+  end
+end
